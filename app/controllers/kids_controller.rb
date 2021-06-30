4925dee18378040a9cadd182519c24084c5af584
dannyirwin <dirwin123@gmail.com>
2021-06-30 10:38:48 -0600

    solution

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -6,13 +6,18 @@ class KidsController < ApplicationController
   end
 
   def create
-    @kid = Kid.create({
-      username: params[:kid][:username],
-      description: params[:kid][:description],
-      favorite_color: params[:kid][:favorite_color],
-      age: params[:kid][:age],
-    })
+    @kid = Kid.new(kid_params)
+    if @kid.valid? 
+      @kid.save
+      render json: { kid: @kid }
+    else
+      render json: @kid.errors.full_messages
+    end
 
-    render json: { kid: @kid }
+  end
+
+  private
+  def kid_params
+    params.require(:kid).permit(:username, :description, :favorite_color, :age)
   end
 end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,4 @@
 class Kid < ApplicationRecord
+    validates :age, inclusion: 7..14, presence: true
+    validates :username, uniqueness: true, presence: true, length: { in: 7...25 }
 end
